934d832e9110b784a217a6ad7bdc953108834d8b
amift <miftahul.dev@gmail.com>
2023-05-12 06:42:15 +0700

    temporary user Home view disable Pengajuan

--- application/modules/user/views/__home/index.php
+++ application/modules/user/views/__home/index.php
@@ -4,7 +4,28 @@
     <section class="content" id="section-data">
         <div class="panel panel-default wrap-table">
             <div class="panel-body">
-                <h1>this is Home User</h1>      
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                      <div class="info-box">
+                        <span class="info-box-icon bg-aqua"><i class="fa fa-book"></i></span>
+                        <div class="info-box-content">
+                          <span class="info-box-text">Jurnal Saya</span>
+                          <span class="info-box-number"><?php echo $myjurnal; ?> </span>
+                        </div>
+                      </div>
+                    </div>
+
+<!--                     <div class="col-md-3 col-sm-6 col-xs-12">
+                      <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="fa fa-pencil-square-o"></i></span>
+                        <div class="info-box-content">
+                          <span class="info-box-text">Pengajuan</span>
+                          <span class="info-box-number">1</span>
+                        </div>
+                      </div>
+                    </div> -->
+                </div>                
+
             </div>
         </div>
     </section>
