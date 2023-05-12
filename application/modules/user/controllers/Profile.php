45e95ec3335bbce73f60109a072590cce64670f9
amift <miftahul.dev@gmail.com>
2023-05-12 07:21:11 +0700

    user profile Edit DONE

---
+++ application/modules/user/controllers/Profile.php
@@ -0,0 +1,100 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends MY_Controller {
+
+	public function __construct(){
+		parent::__construct();
+    	$this->load->helper(['form','string']);
+			$this->load->library(['form_validation']);
+
+      $this->modul='user';
+      $this->class=strtolower(__CLASS__);
+
+      $this->loginCheck();
+      $this->privilegeCheck($this->modul);
+
+      $this->load->model('user_model','m_data');
+      $this->load->model('Provinsi_model','m_provinsi');
+
+    }
+
+	public function index(){
+
+		$invoke['myprofile']    =  $this->m_data->get_it(['id' => $this->session->userdata('ses_id')]);
+		$invoke['arr_provinsi'] = $this->m_provinsi->get_data_as_array('-- Provinsi --');
+
+
+		$invoke['baseurl']      = base_url($this->modul.'/'.$this->class);
+		$invoke['jsfile']       = 'index_js.php';
+
+		$this->load->view('__'.$this->class.'/index', $invoke);
+
+	}
+
+	  public function edit($id=null){
+	      if ($this->input->is_ajax_request()) {      
+	        $data = $this->m_data->get_it(['id'=>$id], 'id, username, name, email, provinsi_id');        
+	        echo json_encode($data);
+	      }else{
+	      	$output=['status' => 'false'];
+	      	$this->output->set_content_type('application/json')->set_output(json_encode($output));
+	      }      
+	  }
+
+		protected function stat_error(){
+			$errors = array(
+				'input_name'      =>  form_error('input_name'),
+				'input_email'     =>  form_error('input_email'),
+				'input_provinsi_id'     =>  form_error('input_provinsi_id'),
+			);
+			return $errors;
+		}
+
+
+    protected function data_post($mode){
+
+    	$data = array(
+				'name'  => html_escape($this->input->post('input_name')),
+				'email'  => html_escape($this->input->post('input_email')),
+				'provinsi_id'  => html_escape($this->input->post('input_provinsi_id')),
+    	);
+			if ($mode=='edit') {
+			}else{
+			}        
+    	return $this->security->xss_clean($data);
+    }
+
+    protected function before_add(){
+    	// add addtional data to core Controller
+    	$data = array();
+    	return $data;
+    }
+
+    protected function after_add($calback){
+    }
+
+    protected function is_related($id){
+    	$decision=false;
+ 			$arr = array(
+ 					 	// $this->m_data->get_relation_data('provinsi','id',$id)
+ 					 );
+    	foreach ($arr as $key => $value) {
+    		$decision=$decision || $value;
+    	}    	
+    	return $decision;
+    }    
+
+		protected function validate($mode=null){
+			$this->set_validation_language();
+			$this->form_validation->set_error_delimiters('','');
+
+			if ($mode=='edit') {
+			}else{
+			}	
+	        $this->form_validation->set_rules('input_name'  ,'Nama' ,'trim|max_length[100]|required');
+	        $this->form_validation->set_rules('input_email'  ,'Email' ,'trim|valid_email|max_length[255]|required');
+	        $this->form_validation->set_rules('input_provinsi_id'  ,'Provinsi' ,'trim|max_length[3]|required');
+		}  
+
+}
---
+++ application/modules/user/views/__profile/index.php
@@ -0,0 +1,90 @@
+<?php $this->load->view('tpl_header'); ?>
+
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1>
+        Profile
+        <small>Profile Saya</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><i class="fa fa-dashboard"></i> Admin</li>
+        <li>User</li>
+      </ol>
+    </section>
+    <section class="content" id="section-data">
+        <div class="panel panel-default wrap-table">
+            <div class="panel-body">
+                <a class ="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit(<?php echo seal_it($myprofile->id) ?>)"><i class="fa fa-edit"></i> </a> 
+                <hr>
+                <dl class="dl-horizontal">
+                    <dt>Nama</dt>      <dd><?php echo $myprofile->name?></dd>
+                    <dt>Username</dt>  <dd><?php echo $myprofile->username?></dd>
+                    <dt>Email</dt>     <dd><?php echo $myprofile->email?></dd>
+                    <dt>Asal Institusi</dt> <dd><?php echo $myprofile->institusi?></dd>
+                    <dt>telepon</dt>   <dd><?php echo $myprofile->telepon?></dd>
+                </dl>                
+            </div>
+        </div>
+    </section>
+
+    <section class="content" id="section-form" style="display: none;">
+        <div class="panel panel-default">
+            <div class="panel-body">
+
+                <form action="#" id="form">
+                    <input type="hidden" value="" name="input_id">
+                    <div class="form-body">
+                        <h3><div id="label"></div></h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <?php
+                                        echo "Nama";
+                                        echo form_input('input_name', set_value('input_name', '', FALSE), 'class="form-control" ');
+                                    ?>
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <?php
+                                        echo "Email";
+                                        echo form_input('input_email', set_value('input_email', '', FALSE), 'class="form-control" ');
+                                    ?>
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <?php 
+                                        echo "Provinsi";
+                                        echo form_dropdown('input_provinsi_id',$arr_provinsi,set_value('input_provinsi_id','',FALSE),'class="form-control" ');
+                                    ?>
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            <div class="panel-footer">
+                <button type="button" class="btn btn-warning" id="btnCancel" onclick="cancel()">
+                    <i class="fa fa-refresh"></i> 
+                    Batal
+                </button>
+                <button type="button" class="btn btn-primary" id="btnSave" onclick="save()">
+                    <i class="fa fa-save"></i>
+                    Simpan
+                </button>
+            </div>
+        </div>
+    </section>            
+</div>
+
+<?php $this->load->view('tpl_footer'); ?>
