66d8b8f896ba006e3a2f3283e5cf35d3633bf148
amift <miftahul.dev@gmail.com>
2023-05-14 12:14:12 +0700

    change field data

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -5,22 +5,22 @@ class User_model extends MY_Model {
     var $table    = 'user';
     var $sql_view = '';
 
-    var $column_order  = array(NULL,NULL,'username','name','email','telepon','provinsi_nama','institusi','last_login'); 
-    var $column_search = array('username','name','email','telepon','provinsi_nama','institusi','last_login'); 
+    var $column_order  = array(NULL,NULL,'username','name','email','telepon','provinsi_nama','institusi', 'is_verified', 'is_active', 'last_login'); 
+    var $column_search = array('username','name','email','telepon','provinsi_nama','institusi', 'is_verified', 'is_active', 'last_login'); 
     var $order         = array('id' => 'ASC');
 
 	public function __construct(){
 		parent::__construct();
     $this->load->database();
 
-		$this->sql_view  = '(SELECT u.id, u.username, u.name, u.email, u.telepon, u.provinsi_id ,p.name as provinsi_nama, u.institusi, u.last_login ';
-		$this->sql_view .= 'FROM user u ';
-		$this->sql_view .= 'LEFT JOIN provinsi p ON p.id = u.provinsi_id) AS sql_view';
+		$this->sql_view  = ' (SELECT u.id, u.username, u.name, u.email, u.telepon, u.provinsi_id ,p.name as provinsi_nama, u.institusi, u.is_verified, u.is_active, u.last_login ';
+		$this->sql_view .= ' FROM user u ';
+		$this->sql_view .= ' LEFT JOIN provinsi p ON p.id = u.provinsi_id) AS sql_view';
 	}
 
 	public function query_data(){
-		$sql  = 'SELECT id, username, name, email, telepon, provinsi_id, provinsi_nama, institusi, last_login ';
-		$sql .= 'FROM '.$this->sql_view;
+		$sql  = ' SELECT id, username, name, email, telepon, provinsi_id, provinsi_nama, institusi, is_verified, is_active, last_login ';
+		$sql .= ' FROM '.$this->sql_view;
 		return $sql;
 	}
 
