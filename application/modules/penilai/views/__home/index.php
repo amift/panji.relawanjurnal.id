8fb88e6aef155fc8cc97500a043d5208b27cb6d0
amift <miftahul.dev@gmail.com>
2023-06-05 12:36:26 +0700

    disable view pengusul

--- application/modules/penilai/views/__home/index.php
+++ application/modules/penilai/views/__home/index.php
@@ -34,19 +34,7 @@
                         </div>
                         <a href="<?php echo base_url('penilai/jurnal') ?> " class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
                       </div>
-                    </div>
-                    <div class="col-lg-3 col-xs-6">
-                      <div class="small-box bg-yellow">
-                        <div class="inner">
-                          <h3><?php echo $pengusul; ?></h3>
-                          <p>Jumlah Pengusul</p>
-                        </div>
-                        <div class="icon">
-                          <i class="fa fa-users"></i>
-                        </div>
-                        <a href="<?php echo base_url('penilai/pengusul') ?> " class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
-                      </div>
-                    </div>                    
+                    </div>                 
                 </div>
             </div>
         </div>
